e001fded76425c9baecc8da8a0870a38b50763d6
mina1112 <mina.kato8885@gmail.com>
2022-08-19 22:00:42 +0900

    first commit

---
+++ confilm.php
@@ -0,0 +1,10 @@
+<?php
+
+$yourname = $_POST["yourname"];
+$email= $_POST["email"];
+$tel = $_POST["tel"];
+$contact = $_POST["contact"];
+
+include "conf.html";
+
+?>
---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('#bar-number').on('inview', function(event, isInView) {
+
+
+        if (isInView) {
+            //要素が見えたときに実行する処理
+            $("#bar-number .bar").addClass('inview');
+            $("#bar-number .count-up").each(function() {
+                $(this).prop('Counter', 0).animate({ //0からカウントアップ
+                    Counter: $(this).data('number')
+                }, {
+                    // スピードやアニメーションの設定
+                    duration: 1800, //数字が大きいほど変化のスピードが遅くなる。2000=2秒
+                    easing: 'swing', //動きの種類。他にもlinearなど設定可能
+                    step: function(now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+        }
+    });
+});
